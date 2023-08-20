b17154c268be91ccdac9f49577ac479622766852
Chris Waitt <chris_waitt@me.com>
2023-08-20 09:52:21 +0000

    Made paths absolute for header.php

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<?php include '/var/www/html/drg/functions.php';?>
+<?php include (__DIR__ . '/functions.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +7,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-<link href="/css/bootstrap-quartz.min.css" rel="stylesheet" >
-<link rel="stylesheet" href="/drg/css/style.css">
+<link href="css/bootstrap.min.css" rel="stylesheet" >
+<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
